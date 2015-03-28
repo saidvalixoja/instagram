909ec022470549558511589be731df028712a41b
@saidvalixoja <saidvalixoja@gmail.com>
2015-03-28 14:50:13 +0100

    ignore files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instagram::Application.config.secret_key_base = '0c5b285d4966138b9da8d6b299c1805d4a53936fe75014b9eaba194cb0198b7128a6e11023395c3e8b9638a324394fd6208c98e78f26491e9e30be9d5cdb7691'
